bdb93c5a3182c6b64ba33d3b18ac4c81054449b0
edgardoburgos94 <32230637+edgardoburgos94@users.noreply.github.com>
2018-09-18 20:27:42 -0500

    Capitalize name

--- solution.rb
+++ solution.rb
@@ -4,7 +4,7 @@ get '/' do
   if params[:nombre] == nil
     @saludo = "Hola desconocido"
   else
-    @saludo = "Hola #{params[:nombre]}"
+    @saludo = "Hola #{params[:nombre].capitalize}"
   end
   # @saludo = "Hola #{params[:nombre]}"
   erb :index
